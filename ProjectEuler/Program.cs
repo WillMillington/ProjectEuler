2d3babae335b11dc760a3db9ee7d21df2385498b
William Millington <WMillington@SpartaGlobal.com>
2020-06-23 09:11:19 +0100

    Fibonacci Task start

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int Fibonacci(int max)
+        {
+            var sequence = new List<int>();
+            int num1 = 1;
+            int num2 = 1;
+            int num3 = 2;
+
+            sequence.Add(num1);
+            //if (max == 1) return sequence;
+            
+            sequence.Add(num2);
+            //if (max == 2) return sequence;
+
+            var evenSequence = new List<int>();
+
+            for (int i = 2; i < max; i++)
+            {                    
+                num3 = num1 + num2;
+                sequence.Add(num3);
+                if (num3 % 2 == 0) evenSequence.Add(num3);
+                num1 = num2;
+                num2 = num3;
+            }
+            int evenSequenceSum = 0;
+            foreach(int i in evenSequence)
+            {
+                evenSequenceSum += i;
+            }
+            return evenSequenceSum;
+            
+        }
+    }
+}
---
+++ ProjectEulerTests/UnitTest1.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+using NUnit.Framework;
+using ProjectEuler;
+
+namespace ProjectEulerTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //[Ignore("Requirements have changed")]
+        //[Test] 
+        //public void Given1_List1()
+        //{
+        //    Assert.That(Program.Fibonacci(1).Count,Is.EqualTo(1));
+        //}
+        //[Ignore("Requirements have changed")]
+        //[TestCase(2)]
+        //[TestCase(3)]
+        //[TestCase(4)]
+        //[TestCase(5)]
+        //public void GivenN_ListN(int n)
+        //{
+        //    Assert.That(Program.Fibonacci(n).Count, Is.EqualTo(n));
+        //}
+        //[Ignore("Requirements have changed")]
+        //[Test]
+        //public void Max_4_Last_Num_is_3()
+        //{
+        //    Assert.That(Program.Fibonacci(4).Last(), Is.EqualTo(3));
+        //}
+        //[Ignore("Requirements have changed")]
+        //[TestCase(2, 1)]
+        //[TestCase(3, 2)]
+        //[TestCase(5, 5)]
+        //[TestCase(6, 8)]
+        //[TestCase(7, 13)]
+        //[TestCase(8, 21)]
+        //public void MaxN_Last_Num_is_correct(int n, int expected)
+        //{
+        //    Assert.That(Program.Fibonacci(n).Last(), Is.EqualTo(expected));
+        //}
+        //[Ignore("Requirements have changed")]
+        //[Test]
+        //public void Get_All_Even_Values()
+        //{
+        //    Assert.That(Program.Fibonacci(8).Count, Is.EqualTo(2));
+        //}
+        [Test]
+        public void Sum_max8_is_10()
+        {
+            Assert.That(Program.Fibonacci(8), Is.EqualTo(10));
+        }
+    }
+}
